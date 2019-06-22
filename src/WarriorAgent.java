3a686d5d219bf2ce6be905606099341e36d6dc18
rjackowski <jackowski_robert@wp.pl>
2019-06-22 17:26:41 +0200

    Warriors

--- src/WarriorAgent.java
+++ src/WarriorAgent.java
@@ -2,7 +2,7 @@ package src;
 
 import jade.core.AID;
 import jade.core.Agent;
-import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.*;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
@@ -10,24 +10,181 @@ import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
+import javax.swing.*;
+import java.awt.*;
+
+
 public class WarriorAgent extends Agent {
+
+
     private int live;
     private int strength;
-    private int location;
 
+    public int getCoinAmount() {
+        return coinAmount;
+    }
+
+    private int coinAmount;
+    private Color color = Color.RED;
+    private boolean existOnMap = false;
+    private boolean startFlag = false;
+    private AID myMapAgent;
+    private RegisterOnMap reg;
+
+    public Color getColor() {
+        return color;
+    }
+
+
+
+    private WarriorAgentGui myGui;
+    private WarriorAgentStateGui myStateGui;
 
 
     protected void setup() {
         System.out.println("Warrior created");
-        addBehaviour(new AskForLocation());
+
+        myGui = new WarriorAgentGui(this);
+        myStateGui = new WarriorAgentStateGui(this);
+        myGui.showGui();
+        //addBehaviour(new AskForLocation());
+
+        //addBehaviour(new RegisterOnMap());
+        reg = new RegisterOnMap();
+
+        addBehaviour(new TickerBehaviour(this, 1000) {
+            protected void onTick() {
+                if(startFlag) {
+                    if (existOnMap == false)
+                        myAgent.addBehaviour(reg);
+                    else {
+                        myAgent.addBehaviour(new SubstrLive());
+                        myStateGui.refreshGui();
+                    }
+                }
+            }
+        });
 
 
     }
 
+    public void start(final int nLive, final int nStrength) {
+        addBehaviour(new OneShotBehaviour() {
+            public void action() {
+                live = nLive;
+                strength = nStrength;
+                startFlag = true;
+            }
+        });
+    }
+
+    public int getLive() {
+        return this.live;
+    }
+
+
+    private class SubstrLive extends Behaviour {
+        public void action() {
+            live = live - 1;
+        }
+
+        public boolean done() {
+            return true;
+        }
+    }
+
+
+
+
+    private class RegisterOnMap extends Behaviour {
+        private int step = 0;
+        private MessageTemplate mt; // The template to receive replies
+
+        public void action() {
+            switch (step) {
+                case 0:
+                    DFAgentDescription template = new DFAgentDescription();
+                    ServiceDescription sd = new ServiceDescription();
+                    sd.setType("map");
+                    System.out.println("Look for map");
+                    template.addServices(sd);
+                    try {
+                        DFAgentDescription[] result = DFService.search(myAgent, template);
+                        System.out.println("Found the following map:");
+
+                        for (int i = 0; i < result.length; ++i) {
+                            myMapAgent = result[i].getName();
+                            System.out.println(myMapAgent.getName());
+                        }
+                    } catch (FIPAException fe) {
+                        fe.printStackTrace();
+                    }
+                    if (myMapAgent != null)
+                        step++;
+
+                break;
+                // Wysłanie zapytania o rejestracje na mapę
+                case 1:
+                    System.out.println("Send register request");
+                    ACLMessage cfp = new ACLMessage(ACLMessage.AGREE);
+                    cfp.addReceiver(myMapAgent);
+                    // cfp.setContent(targetBookTitle);
+                    cfp.setConversationId("register");
+                    cfp.setReplyWith("cfp" + System.currentTimeMillis()); // Unique value
+                    myAgent.send(cfp);
+                    // Prepare the template to get proposals
+                    mt = MessageTemplate.and(MessageTemplate.MatchConversationId("register"),
+                            MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+                    step ++;
+                    break;
+                //Odebranie potwierdzenia i przydzielonego koloru
+                case 2:
+                    System.out.println("Receive");
+                    ACLMessage reply = myAgent.receive(mt);
+                    if (reply != null) {
+                        // Reply received
+                        if (reply.getPerformative() == ACLMessage.AGREE) {
+                            try {
+                                color = (Color)Color.class.getField(reply.getContent()).get(null);
+                            }catch(Exception ex) {
+
+                            }
+                        }
+
+                        if (color!= null) {
+                            System.out.println("color Done");
+                            existOnMap = true;
+                            step ++;
+                        }
+                    }
+                    else {
+                        block();
+                    }
+                    existOnMap = true;
+                    break;
+            }
+        }
+
+        @Override
+        public boolean done() {
+
+            if (step == 3) {
+                System.out.println("Done");
+                return true;
+            }
+
+            else
+                return false;
+        }
+    }
+
+
     private class AskForLocation extends Behaviour {
-        private AID bestSeller; // The agent who provides the best offer
-        private int bestPrice;  // The best offered price
-        private int repliesCnt = 0; // The counter of replies from seller agents
+        //private AID map;
+
+        //private AID bestSeller; // The agent who provides the best offer
+        //private int bestPrice;  // The best offered price
+        //private int repliesCnt = 0; // The counter of replies from seller agents
         private MessageTemplate mt; // The template to receive replies
         private int step = 0;
         private AID[] map;
@@ -50,11 +207,9 @@ public class WarriorAgent extends Agent {
                     myAgent.send(question);
                     System.out.println(map[i].getName());
                 }
-            }
-            catch (FIPAException fe) {
+            } catch (FIPAException fe) {
                 fe.printStackTrace();
             }
-
 
 
 //
@@ -66,8 +221,6 @@ public class WarriorAgent extends Agent {
             return true;
         }
     }  // End of inner class RequestPerformer
-
-
 
 
 }
